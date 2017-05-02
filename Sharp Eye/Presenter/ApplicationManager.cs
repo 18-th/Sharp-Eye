5155f5e2bdc1c8f93f40f89442ee435637154363
shukur <shukur.tomsk@gmail.com>
2017-05-02 19:51:21 +0700

    Создал классы ApplicationManager, ContainerControl

---
+++ Sharp Eye/Presenter/ApplicationManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presenter
+{
+    /// <summary>
+    /// Класс управляет логикой вызоовов презентеров и отображением контролов
+    /// </summary>
+    public class ApplicationManager
+    {
+
+    }
+}
---
+++ Sharp Eye/View/ContainerControl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using View.Interfaces;
+
+namespace View
+{
+    /// <summary>
+    /// Служит контайнером для всех контролов
+    /// </summary>
+    public partial class ContainerControl : UserControl, IContainerView
+    {
+        public ContainerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
