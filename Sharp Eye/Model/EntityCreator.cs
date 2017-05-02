94e033ad710368ac054fa26d9b06d7ce6dcd25c3
shukur <shukur.tomsk@gmail.com>
2017-05-02 19:40:05 +0700

    Создал класс EntityCreator

---
+++ Sharp Eye/Model/EntityCreator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Interfaces;
+
+namespace Model
+{
+    /// <summary>
+    /// Класс выполняет внедрение зависимости в слои презентера и вью.
+    /// </summary>
+    public class EntityCreator
+    {
+        /// <summary>
+        /// Метод вызывается во время загрузки приложения.
+        /// Испорльзуйте его если вам необходимо выполнить инициаллизацию ваших
+        /// компонентов.
+        /// </summary>
+        public static void Initialize()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Объек, выполняющий соединение с сервером</returns>
+        public static IConnectionModel ConnectionModelBuild()
+        {
+            throw new Exception();
+        }
+    }
+}
