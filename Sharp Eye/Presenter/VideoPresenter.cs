d95b5d6fcd0cc69e32231c14d5068c6e3321c1cc
shukur <shukur.tomsk@gmail.com>
2017-05-03 21:58:56 +0700

    Бэкап по просьбе Вани

--- Sharp Eye/Presenter/VideoPresenter.cs
+++ Sharp Eye/Presenter/VideoPresenter.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Presenter.Interfaces;
 using View.Interfaces;
 
 namespace Presenter
 {
-    class VideoPresenter
+    class VideoPresenter: IVideoPresenter
     {
         private IVideoView _view;
-        public VideoPresenter()
+        public VideoPresenter(IVideoView view)
         {
+            if(view != null)
+            {
+                _view = view;
+            }
+        }
 
+        public IVideoView GetView()
+        {
+            return _view;
         }
     }
 }
--- Sharp Eye/View/VideoControl.cs
+++ Sharp Eye/View/VideoControl.cs
@@ -7,10 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using View.Interfaces;
 
 namespace View
 {
-    public partial class VideoControl : UserControl
+    public partial class VideoControl : UserControl, IVideoView
     {
         public VideoControl()
         {
